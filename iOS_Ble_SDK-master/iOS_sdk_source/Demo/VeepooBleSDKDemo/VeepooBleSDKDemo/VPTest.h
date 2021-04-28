e3ca138baa4ae31fcb7a36d326167397b002d9ff
Chandresh2021 <81814883+Chandresh2021@users.noreply.github.com>
2021-04-29 02:09:44 +0530

    Update VPTest.h

--- iOS_Ble_SDK-master/iOS_sdk_source/Demo/VeepooBleSDKDemo/VeepooBleSDKDemo/VPTest.h
+++ iOS_Ble_SDK-master/iOS_sdk_source/Demo/VeepooBleSDKDemo/VeepooBleSDKDemo/VPTest.h
@@ -3,7 +3,7 @@
 //  VeepooBleSDKDemo
 //
 //  Created by Zhang Chong on 17/6/1.
-//  Copyright © 2017年 zc.All rights reserved.
+//  Copyright © 2017 year zc.All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
