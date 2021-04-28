03b32003a3bdb05e8b4299ced3981ad1a5407847
Chandresh2021 <81814883+Chandresh2021@users.noreply.github.com>
2021-04-29 05:17:12 +0530

    Update MJFoundation.h

--- iOS_Ble_SDK-master/iOS_sdk_source/Demo/VeepooBleSDKDemo/Pods/MJExtension/MJExtension/MJFoundation.h
+++ iOS_Ble_SDK-master/iOS_sdk_source/Demo/VeepooBleSDKDemo/Pods/MJExtension/MJExtension/MJFoundation.h
@@ -3,7 +3,7 @@
 //  MJExtensionExample
 //
 //  Created by MJ Lee on 14/7/16.
-//  Copyright (c) 2014年 小码哥. All rights reserved.
+//  Copyright (c) 2014 year Little Code. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
